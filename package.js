e21f95dd1e9013b0adcb4e6022ebaea58bb13cfb
omly <515275925@qq.com>
2017-01-03 11:32:53 +0800

    this is first push

---
+++ package.js
@@ -0,0 +1,10 @@
+﻿var config = {
+	"name": "测试应用",
+	"description": "",
+	"version": "1.0",
+	"keyword": "004",
+	"serverip": "192.168.128.123",
+	"serverport": "8080",
+	"imageobj": [],
+	"mediaobj": []
+}
